f359daafda286ea01995026aca08d74ef8c00436
Tacotime2 <150088820+Tacotime2@users.noreply.github.com>
2023-11-29 10:53:48 -0600

    Feat: Added map

---
+++ Source/Player.h
@@ -0,0 +1,49 @@
+#pragma once
+#include <iostream>
+using namespace std;
+
+class Player
+{
+private:
+    int x, y; //Player's position
+
+public:
+    Player(int initialX, int initialY) {
+        x = initialX;
+        y = initialY;
+    }
+
+    void handleInput() {
+        char input;
+        cout << "Enter move (WASD): ";
+        cin >> input;
+
+        switch (input) {
+        case 'W': case 'w':
+            move(0, -1);
+            break;
+        case 'S': case 's':
+            move(0, 1);
+            break;
+        case 'A': case 'a':
+            move(-1, 0);
+            break;
+        case 'D': case 'd':
+            move(1, 0);
+            break;
+            //Add more controls as needed
+        }
+    }
+
+    void move(int dx, int dy) {
+        x += dx;
+        y += dy;
+    }
+
+    void getPosition(int& outX, int& outY) const {
+        outX = x;
+        outY = y;
+    }
+
+};
+
--- Source/hello-world.cpp
+++ Source/hello-world.cpp
@@ -1,11 +1,28 @@
 // Date: 11/27/2023
-// Auth: John O'Neal
-// Desc: Hello World program for C++.
+// Auth: John O'Neal, William Brickner, Christian Botos, Hunter Kauffman, Christian Baack.
+// Desc: Main for final project.
 
 #include <iostream>
-using namespace std;
+#include "Map.h"
+#include "Player.h"
 
 int main() {
-    cout << "Hello World!" << endl;
+    //Game objects
+    Map gameMap(10, 10);
+    Player player(0, 0);
+
+    //Infinite loop right now, will need a menu.
+    while (true) {
+        player.handleInput();
+        //This clears the cli
+        system("cls");
+        //Update player's position on the map
+        int playerX, playerY;
+        player.getPosition(playerX, playerY);
+        gameMap.updatePlayerPosition(playerX, playerY);
+        gameMap.display();
+
+    }
+
     return 0;
 }
