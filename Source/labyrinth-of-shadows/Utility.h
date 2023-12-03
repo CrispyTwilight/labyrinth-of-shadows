5b7ddc5ad6050bf8cdc6be78df870646d7937ecb
Tacotime2 <150088820+Tacotime2@users.noreply.github.com>
2023-12-03 16:55:56 -0600

    Walls, maps, and formatting
    Feat: added map manager
    Chore: Did some formatting
    Feat: Made collisions with walls

--- Source/labyrinth-of-shadows/Utility.h
+++ Source/labyrinth-of-shadows/Utility.h
@@ -2,6 +2,7 @@
 //Date: 11/30/23
 //Desc: All of the utility function and modules that are used most or all classes that will free up space and get rid of clutter.
 #pragma once
+#include <windows.h>
 
 class Utility
 {
@@ -14,4 +15,24 @@ class Utility
         };
     }
     */
+
+public:
+    //This is for some visual issues I was having with the cli, Should be moved later to utilities.
+    //This always keeps the command line at the bottom instead of clearing it.
+    void setCursorPosition(int x, int y)
+    {
+        static const HANDLE hOut = GetStdHandle(STD_OUTPUT_HANDLE);
+        std::cout.flush();
+        COORD coord = { (SHORT)x, (SHORT)y };
+        SetConsoleCursorPosition(hOut, coord);
+    }
+
+    void visual()
+    {
+        //Also for visual issues, will be moved.
+        CONSOLE_CURSOR_INFO cursorInfo;
+        GetConsoleCursorInfo(GetStdHandle(STD_OUTPUT_HANDLE), &cursorInfo);
+        cursorInfo.bVisible = false; // Set cursor visibility
+        SetConsoleCursorInfo(GetStdHandle(STD_OUTPUT_HANDLE), &cursorInfo);
+    }
 };
