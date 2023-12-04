b01a755c777cc2aa7aec5d25cefb71c3f0547d3d
John O'Neal <117692471+CrispyTwilight@users.noreply.github.com>
2023-12-04 12:00:18 -0600

    feat: create GameManager.h
    A new file with placeholder functions.

---
+++ Source/labyrinth-of-shadows/GameManager.h
@@ -0,0 +1,50 @@
+// Date: 12/04/2023
+// Auth: John O'Neal
+// Desc: This class will manage the game and all of its components. Loading, ending, saving, quitting, etc. will all be handled here.
+#pragma once
+
+// Will need to #include all of the other classes here.
+
+class GameManager
+{
+public:
+    // Initializes the game, loads resources, etc.
+    void init() {
+
+    }
+
+    // Starts the main game loop
+    void start() {
+
+    }
+
+    // Handles the game's update logic
+    void update() {
+
+    }
+
+    // Renders the game to the screen
+    void render() {
+
+    }
+
+    // Ends the game with message, releases resources, etc.
+    void end() {
+
+    }
+
+    // Saves the current game state
+    void save() {
+
+    }
+
+    // Loads a saved game state
+    void load() {
+
+    }
+
+    // Quits the game
+    void SaveAndQuit() {
+
+    }
+};
