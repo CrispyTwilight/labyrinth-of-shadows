6e66edf528a6a579e092ad7239ed79b498d3ffa7
Nukester1000 <nukester315@gmail.com>
2023-12-03 20:06:55 -0600

    feat: add Dice class

---
+++ Source/labyrinth-of-shadows/Dice.h
@@ -0,0 +1,67 @@
+#pragma once
+//Auth: Hunter Kauffman
+//Date: 11/30/23
+//Desc: Dice class for generating random numbers based on the max and min number put in
+
+#include <iostream>
+#include <ctime>
+using namespace std;
+
+class Dice
+{
+private:
+	//Fields
+	int max;
+	int min;
+	int prevRoll;
+
+public:
+	//Constructor
+	Dice(int mx = 0, int mn = 0, int pr = 0)
+	{
+		setMax(mx);
+		setMin(mn);
+		setPrevRoll(pr);
+	}
+
+	//Getters
+	int getMax()
+	{
+		return max;
+	}
+
+	int getMin()
+	{
+		return min;
+	}
+
+	int getPrevRoll()
+	{
+		return prevRoll;
+	}
+
+	//Setters
+	void setMax(int mx)
+	{
+		max = mx;
+	}
+
+	void setMin(int mn)
+	{
+		min = mn;
+	}
+
+	void setPrevRoll(int pr)
+	{
+		prevRoll = pr;
+	}
+
+	//Methods
+	int rollDice()
+	{
+		srand(time(0));
+		setPrevRoll(rand() % max + min);
+		return prevRoll;
+	}
+};
+
