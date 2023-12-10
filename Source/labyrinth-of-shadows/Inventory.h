613fdcf90724d1b320019fcae6ccfd509ad66b01
botosc <151964733+botosc@users.noreply.github.com>
2023-12-10 11:25:18 -0600

    feat: add screens class
    Added screens to display to the user for the title screen, victory screen, loss screen, and help screen

--- Source/labyrinth-of-shadows/Inventory.h
+++ Source/labyrinth-of-shadows/Inventory.h
@@ -2,8 +2,12 @@
 // Date: 12/07/2023
 // Desc: header file for the Inventory class for the player's inventory
 #pragma once
+<<<<<<< Updated upstream
 #include "All_Includes.h"
 #include "Item.h" //JPO: For some reason, this is needed here again?
+=======
+#include "Headers.h"
+>>>>>>> Stashed changes
 
 class Inventory
 {
@@ -14,6 +18,7 @@ private:
 
 public:
 	// constructor
+<<<<<<< Updated upstream
 	Inventory(int initialGold = 100) : gold(initialGold) {}
 
 	// getters
@@ -40,6 +45,46 @@ public:
 	void addItem(Item* item) {
 		items.push_back(item);
 	}
+=======
+	Inventory(int initialGold = 0) : gold(initialGold) {}
+
+	// getters
+	int getGold() const
+	{
+		return gold;
+	}
+
+	const vector<Item*>& getItems() const
+	{
+		return items;
+	}
+
+	// setters
+	template <typename T>
+	void addItem(T* item)
+	{
+		items.push_back(static_cast<Item*>(item));
+	}
+
+	void removeItem(int index)
+	{
+		if (index >= 0 && index < items.size())
+		{
+			items.erase(items.begin() + index);
+		}
+	}
+
+	void addGold(int g)
+	{
+		gold += g;
+	}
+
+	void deductGold(int g)
+	{
+		gold -= g;
+	}
+};
+>>>>>>> Stashed changes
 
 	void removeItem(int index) {
 		items.erase(items.begin() + index);
