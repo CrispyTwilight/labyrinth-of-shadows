bbce2e43c382b64f46e715b81504c4b9970c0982
John O'Neal <117692471+CrispyTwilight@users.noreply.github.com>
2023-12-11 13:45:35 -0600

    Feat: updated screen functionality
    also modded the main()

--- Source/labyrinth-of-shadows/labyrinth-of-shadows.cpp
+++ Source/labyrinth-of-shadows/labyrinth-of-shadows.cpp
@@ -5,16 +5,16 @@
 
 // Prototypes
 void testDriver();
-void mapDriver();
+void gameDriver();
 void shopDriver();
+void screenDriver();
+
 
 // Initialization stuff
 Shop* Shop::instance = nullptr; // JPO: Needed to initialize the static instance variable for the shop.
-GameManager game;
 
 int main()
 {
-   
     srand(time(0)); // JPO: Moved from Dice.h to so  it is only called once. Was causing issues with randomness in the shop.
     testDriver();
     return 0;
@@ -22,42 +22,45 @@ int main()
 
 // JPO: This function asks which test to drive.
 void testDriver() {
-    char choice = '0'; // Default value to enter the loop
-
-    // Start a loop that will run until the user presses q
-    while (choice != 'q') {
+    char choice;
+    do {
         // Welcome and menu
         cout << "Pick an option to test:\n"
-            << "1. Test Map\n"
-            << "2. Test Shop\n"
-            << "q. Quit\n"
+            << "1) Test full game\n"
+            << "2) Test Shop\n"
+            << "3) Test Screens\n"
+            << "ESC) Quit\n"
             << "Choice: ";
         choice = _getch();
 
         switch (choice) {
-        case '1':
-            mapDriver();
-            break;
-        case '2':
-            shopDriver();
-            break;
-        case 'q':
-            cout << "Quitting..." << endl;
-            return;
-            break;
-        default:
-            cout << "Invalid choice. Try again." << endl;
-            break;
-        }
-        if (choice != 'q') {
+            case '1':
+                gameDriver();
+                break;
+            case '2':
+                shopDriver();
+                break;
+            case '3':
+                system("cls");
+                screenDriver();
+			    break;
+            case 27:
+                cout << "Quitting..." << endl;
+                return;
+            default:
+                cout << "Invalid choice. Try again." << endl;
+                break;
+            }
+        if (choice != 27) {
             system("pause");
             system("cls");
         }
-    }
+    } while (true);
 }
 
 // JPO: This function drives the map.
-void mapDriver() {
+void gameDriver() {
+    GameManager game;
     //Game objects
     Map gameMap(51, 31); // JPO: Updated so that the @ is can actually be in the middle of the map.
     Player player(gameMap, 25, 15); // JPO: Updated so that the player starts in the middle of the map.
@@ -93,7 +96,7 @@ void mapDriver() {
 
             //Calls the fighting.
             game.fighting(false);
-            
+
             gameMap.toggle();
         }
         if (gameMap.getTrigger2())
@@ -110,4 +113,43 @@ void mapDriver() {
 void shopDriver() {
     cout << endl << endl;
     Shop::getInstance()->runShop();
+}
+
+void screenDriver() {
+    Screens screens;
+    char choice;
+    do {
+        cout << "Pick an option to test:\n"
+            << "1) Title Screen\n"
+            << "2) Death Screen\n"
+            << "3) Victory Screen\n"
+            << "4) Help Screen\n"
+            << "ESC) Quit\n";
+
+        choice = _getch();
+        switch (choice) {
+        case '1':
+            screens.showTitle();
+            screens.processMainMenu();
+            break;
+        case '2':
+            screens.showDeath();
+            break;
+        case '3':
+            screens.showVictory();
+            break;
+        case '4':
+            screens.showHelp();
+            break;
+        case 27 :
+            return;
+        default:
+            cout << "Invalid choice. Try again." << endl;
+            break;
+        }
+        if (choice != 27) {
+			system("pause");
+			system("cls");
+		}
+    } while (choice != 27);
 }
